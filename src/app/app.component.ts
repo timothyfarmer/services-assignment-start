ed7fa512bc5dfb0626feb0d2bad706d2007131b5
unknown <tfarmer@ad.bfinancedta.com>
2018-01-23 11:41:06 -0600

    cleaning up unused and unnecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {UserService} from "./shared/user.service";
 
 @Component({
   selector: 'app-root',
@@ -7,20 +6,7 @@ import {UserService} from "./shared/user.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
 
-  constructor(private userService: UserService) {}
+  constructor() {}
 
-  onSetToInactive(id: number) {
-    this.userService.setUserInactive(id);
-    this.activeUsers = this.userService.activeUsers;
-  }
-
-  onSetToActive(id: number) {
-    // this.activeUsers.push(this.inactiveUsers[id]);
-    // this.inactiveUsers.splice(id, 1);
-    this.userService.setUserActive(id);
-    this.inactiveUsers = this.userService.inactiveUsers;
-  }
 }
